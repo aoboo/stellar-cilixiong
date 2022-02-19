0559afc12e71e8dcfa3af2f68d3eaf5a61986742
nobody <nobody@qq.com>
2022-02-19 11:39:56 +0800

    version2

--- main.py
+++ main.py
@@ -335,6 +335,29 @@ class m66ysplugin(StellarPlayer.IStellarPlayerPlugin):
     def loadingPage(self, page, stopLoading = False):
         if hasattr(self.player,'loadingAnimation'):
             self.player.loadingAnimation(page, stop=stopLoading)
+
+    def onPlayerSearch(self, dispatchId, searchId, wd, limit):
+        # 播放器搜索异步接口
+        print(f'onPlayerSearch:{wd}')
+        result = []
+        if len(self.search_urls) > 0:
+            url = self.search_urls[0]
+            movies = search_cilixiong_page_movies(url,wd)
+            for item in movies:
+                magnets = parse_cilixiong_movie(item['url'])
+                if len(magnets) > 0:
+                    urls = []
+                    index = 1
+                    for magnet in magnets:
+                        obj = []
+                        obj.append('磁力' + str(index))
+                        obj.append(magnet['url'])
+                        urls.append(obj)
+                        index = index + 1
+                    result.append({'urls':urls,'name':item['title'],'pic':item['img']})
+                if len(result) >= limit:
+                    break
+        self.player.dispatchResult(dispatchId, searchId=searchId, wd=wd, result=result)
     
 def newPlugin(player:StellarPlayer.IStellarPlayer,*arg):
     plugin = m66ysplugin(player)
